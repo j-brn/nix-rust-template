b8c9a580a45d0bb0be8afe6ca80e2a1fd3b63dde
j-brn <me@bricker.io>
2022-11-29 17:50:11 +0100

    replace flake-utils with flake-parts

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     naersk = {
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,31 +12,40 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        toolchain = fenix.packages.${system}.fromToolchainFile {
-          file = ./rust-toolchain.toml;
-          sha256 = "sha256-DzNEaW724O8/B8844tt5AVHmSjSQ3cmzlU4BP90oRlY=";
-        };
-        naersk-lib =
-          (naersk.lib.${system}.override {
-            cargo = toolchain;
-            rustc = toolchain;
-          });
-      in
-      {
-        packages.default = naersk-lib.buildPackage {
-          src = ./.;
-        };
+  outputs = { self, fenix, naersk, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit self; } ({ withSystem, ... }: {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            nixpkgs-fmt
-            toolchain
-          ];
+      perSystem = { lib, config, self', inputs', pkgs, system, ... }:
+        let
+          toolchain = fenix.packages.${system}.fromToolchainFile {
+            file = ./rust-toolchain.toml;
+            sha256 = "sha256-DzNEaW724O8/B8844tt5AVHmSjSQ3cmzlU4BP90oRlY=";
+          };
+
+          naersk-lib =
+            (naersk.lib.${system}.override {
+              cargo = toolchain;
+              rustc = toolchain;
+            });
+        in
+        {
+          packages.default = naersk-lib.buildPackage {
+            src = ./.;
+          };
+
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              nixpkgs-fmt
+              toolchain
+            ];
+          };
         };
-      });
+    });
 }
